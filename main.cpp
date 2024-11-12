a50df7ab7570ef01e51cdf31bc3764cab0e4d18c
qyh <1466821178@qq.com>
2024-11-12 13:51:41 +0800

    update

--- main.cpp
+++ main.cpp
@@ -3,20 +3,20 @@
 #include "programs.h"
 
 
-void print_info(void) {
-	std::cout << "Publications associated with this program:" << std::endl;
-	std::cout << "J. Tachella et al., ''Real-time 3D reconstruction from single-photon lidar data using plug-and-play-denoisers'', Nature Communications, vol. 10, pp. 4984, November 2019" << std::endl;
-	std::cout << "J. Tachella et al., ''Real-time color 3D reconstruction from single-photon lidar data'', in Proc. International Workshop on Computational Advances in Multi Sensor Adaptive Processing (CAMSAP), Guadaloupe, West Indies, December 2019" << std::endl;
+// void print_info(void) {
+// 	std::cout << "Publications associated with this program:" << std::endl;
+// 	std::cout << "J. Tachella et al., ''Real-time 3D reconstruction from single-photon lidar data using plug-and-play-denoisers'', Nature Communications, vol. 10, pp. 4984, November 2019" << std::endl;
+// 	std::cout << "J. Tachella et al., ''Real-time color 3D reconstruction from single-photon lidar data'', in Proc. International Workshop on Computational Advances in Multi Sensor Adaptive Processing (CAMSAP), Guadaloupe, West Indies, December 2019" << std::endl;
 
-}
-void print_copyright(void) {
-	std::cout << "Author: Julian Tachella, Heriot-Watt University, Edinburgh, UK. TeSA Laboratory, Toulouse, France. Contact: jat3@hw.ac.uk // tachella.github.io" << std::endl << std::endl;
-}
+// }
+// void print_copyright(void) {
+// 	std::cout << "Author: Julian Tachella, Heriot-Watt University, Edinburgh, UK. TeSA Laboratory, Toulouse, France. Contact: jat3@hw.ac.uk // tachella.github.io" << std::endl << std::endl;
+// }
 
 int main()
 {
-	print_info();
-	print_copyright();
+	// print_info();
+	// print_copyright();
 
 	//run_all();
 	run_single();
